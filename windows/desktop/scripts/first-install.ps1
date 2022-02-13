4048399143100ffc8022ef13ff89e22052a6b7de
jmadoremos <21135564+jmadoremos@users.noreply.github.com>
2022-02-13 22:01:40 +0800

    Added sudoer, upate and python install

--- windows/desktop/scripts/first-install.ps1
+++ windows/desktop/scripts/first-install.ps1
@@ -1,2 +1,16 @@
 # Install Debian distro in Windows Subsystem for Linux
 wsl --install -d Debian
+
+# Login to wsl to setup credentials
+wsl
+
+# Create sudoer file for the current user
+wsl -- echo "$(whoami)  ALL=(ALL)  NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/$(whoami)"
+
+# Update the system
+wsl -- sudo apt update
+
+wsl -- sudo apt dist-upgrade -y
+
+# Install python3 and pip3
+wsl -- sudo apt install -y python3 python3-pip
